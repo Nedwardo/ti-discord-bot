8afc7fcbfcfe511acabe8931ea833d1714e703f0
Nedwardo <edwardbo123@gmail.com>
2025-08-04 21:49:23 +0100

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,14 @@ interface ENV {
 }
 export default {
   async fetch(request: Request, env: ENV) {
+    console.log("Setting up db")
     const db = drizzle(env.db, {schema})
 
+    console.log("Handling response")
     const response = await handle_discord_commands(request, env.DISCORD_TOKEN, db);
 
     if (response._tag === "Success"){
+      console.log("Success!\nResponding with:\n" + JSON.stringify(response.data))
       return new Response(JSON.stringify(response.data), {headers: { 'content-type': 'application/json' }})
     }
 
