96e87538a483d596b6af86294b02c049b2ff1c1b
Nedwardo <edwardbo123@gmail.com>
2025-08-05 14:22:25 +0100

    Fixed comparison

--- src/utils/get_username_from_ids.ts
+++ src/utils/get_username_from_ids.ts
@@ -5,10 +5,7 @@ async function get_username_from_id(id: string) : Promise<string>{
         response => {
             try{
                 const username = response.json().then(json_response => {
-                    console.log("Response = " + JSON.stringify(json_response))
-                    console.log("username" in Object.keys(json_response))
-                    console.log("username" in Object.keys(json_response) ? json_response.username as string : id)
-                    return "username" in Object.keys(json_response) ? json_response.username as string : id
+                    return Object.keys(json_response).includes("username") ? json_response.username as string : id
                 })
                 return username
             } catch{
